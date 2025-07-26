b424bddeae0e3a40bfdbba59338691a697817ae4
Ouedraogo Bonaventure Cheick Hamed <bouedraogo0412@gmail.com>
2025-07-26 17:50:51 +0000

    modification texte d'avertissement

--- src/components/verification.tsx
+++ src/components/verification.tsx
@@ -8,7 +8,7 @@ function Verification({ props }: VerificationProps) {
   return (
     <div className="flex w-screen h-screen inset-0 items-center justify-center z-50">
       <div className="relative z-20 border border-blue-500 py-20 px-14 flex flex-col align-top rounded-xl text-center">
-        <p className="text-xl font-medium my-9 flex-wrap">Cette page contient du contenue sexuel qui serait inadapté pour certainnes personne<span className="text-red-500">Avez vous 18 ans ?</span></p>
+        <p className="text-xl font-medium my-9 flex-wrap">Cette page contient du contenue sexuel qui pourrait être inadapté pour certainnes personne <span className="text-red-500">Avez vous 18 ans ?</span></p>
         <div>
           <button
             className="bg-blue-600 py-3 px-5 rounded-lg mx-3"
